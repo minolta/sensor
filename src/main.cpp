2e4d4bc85edeea53021747415209823411dabca1
Kritchai Phromros <ky@pixka.me>
2025-08-29 18:09:47 +0700

    upadte buffer

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ Htask *hservice = new Htask();
 // The serial connection to the GPS device
 PZEM004Tv30 pzem(&Serial);
 SoftwareSerial ss(RXPin, TXPin);
-const String version = "173";
+const String version = "174";
 #define xs 40
 #define ys 15
 #define pingPin D1
@@ -954,7 +954,7 @@ time_t realtime()
 }
 String makeStatus()
 {
-    int buffersize = 2000;
+    int buffersize = 2500;
     cfg.setbuffer(configdata.jsonbuffer);
     DynamicJsonDocument doc(buffersize);
     doc["description"] = configdata.description;
