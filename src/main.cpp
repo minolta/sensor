be11acb36f71a67edb4b0e5eaf45d0d9f0324b2e
Kritchai Phromros <ky@pixka.me>
2025-09-06 18:06:37 +0700

    support tm

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ Htask *hservice = new Htask();
 // The serial connection to the GPS device
 PZEM004Tv30 pzem(&Serial);
 SoftwareSerial ss(RXPin, TXPin);
-const String version = "174";
+const String version = "175";
 #define xs 40
 #define ys 15
 #define pingPin D1
